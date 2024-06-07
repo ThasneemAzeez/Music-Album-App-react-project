ee407cd38889c4564ca3cbcc6882688d4e387937
ThasneemAzeez <thasneemazeez2001@gmail.com>
2024-06-08 00:16:17 +0530

    navigation modified

--- src/App.js
+++ src/App.js
@@ -4,14 +4,19 @@ import FrontPage from './components/FrontPage';
 import SearchMusic from './components/SearchMusic';
 import Delete from './components/Delete';
 import Viewall from './components/Viewall';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div >
-      <FrontPage/>
-      <SearchMusic/>
-      <Delete/>
-      <Viewall/>
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<FrontPage/>}/>
+        <Route path='/Search' element={<SearchMusic/>}/>
+        <Route path='/Delete' element={<Delete/>}/>
+        <Route path='/Viewall' element={<Viewall/>}/>
+      </Routes>
+      </BrowserRouter>
       
       
     </div>
