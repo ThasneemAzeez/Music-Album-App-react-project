7dd5a1509b542c59c9fd87e91ec6aea7b287c380
ThasneemAzeez <thasneemazeez2001@gmail.com>
2024-06-08 00:20:01 +0530

    navigation modified

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Navigation = () => {
     return (
@@ -7,23 +8,23 @@ const Navigation = () => {
                 <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                     <nav class="navbar navbar-expand-lg bg-body-tertiary">
                         <div class="container-fluid">
-                            <a class="navbar-brand" href="#">Music Album</a>
+                            <Link class="navbar-brand" to="/">Music Album</Link>
                             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                                 <span class="navbar-toggler-icon"></span>
                             </button>
                             <div class="collapse navbar-collapse" id="navbarNav">
                                 <ul class="navbar-nav">
                                     <li class="nav-item">
-                                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                                        <Link class="nav-link active" aria-current="page" to="/">Home</Link>
                                     </li>
                                     <li class="nav-item">
-                                        <a class="nav-link" href="#">Search</a>
+                                        <Link class="nav-link" to="/Search">Search</Link>
                                     </li>
                                     <li class="nav-item">
-                                        <a class="nav-link" href="#">Delete</a>
+                                        <Link class="nav-link" href="/Delete">Delete</Link>
                                     </li>
                                     <li class="nav-item">
-                                        <a class="nav-link " href="">View All</a>
+                                        <Link class="nav-link " href="/Viewall">View All</Link>
                                     </li>
                                 </ul>
                             </div>
